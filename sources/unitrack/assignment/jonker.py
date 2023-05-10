b67470db92c67017ec3ab8ba4208d877789d72ec
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2023-05-10 12:11:12 +0200

    Refactored assignment procedure

--- sources/unitrack/assignment/jonker.py
+++ sources/unitrack/assignment/jonker.py
@@ -11,37 +11,38 @@ __all__ = ["Jonker"]
 
 class Jonker(Assignment):
     def _assign(self, cost_matrix: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        return self.linear_assignment(cost_matrix)
+        return jonker_volgenant_assignment(cost_matrix)
 
-    @torch.jit.ignore()
-    @torch.no_grad()
-    def linear_assignment(self, cost_matrix: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Perform linear assignment. If possible, an assignment on the diagonal of the
-        matrix is preferred if this assignment has equal cost to the algorithm
-        result.
 
-        TODO: PyTorch implementation
-        """
+@torch.jit.ignore()
+@torch.no_grad()
+def jonker_volgenant_assignment(cost_matrix: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Perform linear assignment. If possible, an assignment on the diagonal of the
+    matrix is preferred if this assignment has equal cost to the algorithm
+    result.
 
-        cost_matrix = cost_matrix.detach().cpu().numpy()
+    TODO: PyTorch implementation
+    """
 
-        matches, unmatched_a, unmatched_b = [], [], []
+    cost_matrix = cost_matrix.detach().cpu().numpy()
 
-        # Jonker algorithm, i.e. linear sum assignment (rows) -> (cols)
-        cost, x, y = lapjv(cost_matrix, extend_cost=True)
+    matches, unmatched_a, unmatched_b = [], [], []
 
-        # Create match and unassigned lists
-        for ix, mx in enumerate(x):
-            if mx >= 0:
-                matches.append([ix, mx])
+    # Jonker algorithm, i.e. linear sum assignment (rows) -> (cols)
+    cost, x, y = lapjv(cost_matrix, extend_cost=True)
 
-        unmatched_a = np.where(x < 0)[0]
-        unmatched_b = np.where(y < 0)[0]
-        matches = np.asarray(matches)
+    # Create match and unassigned lists
+    for ix, mx in enumerate(x):
+        if mx >= 0:
+            matches.append([ix, mx])
 
-        return (
-            torch.from_numpy(matches).long(),
-            torch.from_numpy(unmatched_a).long(),
-            torch.from_numpy(unmatched_b).long(),
-        )
+    unmatched_a = np.where(x < 0)[0]
+    unmatched_b = np.where(y < 0)[0]
+    matches = np.asarray(matches)
+
+    return (
+        torch.from_numpy(matches).long(),
+        torch.from_numpy(unmatched_a).long(),
+        torch.from_numpy(unmatched_b).long(),
+    )
