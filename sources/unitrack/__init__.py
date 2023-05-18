65ebc64f49a543ae1c556e2cf9ef38eb0302878c
Kurt Stolle <kurt-stolle@users.noreply.github.com>
2023-05-18 22:50:57 +0200

    Expose assignment modules via main package

--- sources/unitrack/__init__.py
+++ sources/unitrack/__init__.py
@@ -25,7 +25,7 @@ Lost
 
 __version__ = "4.5.0"
 
-from . import costs, fields, stages, states  # noqa: F401
+from . import assignment, costs, fields, stages, states  # noqa: F401
 from .context import *  # noqa: F401, F403
 from .memory import *  # noqa: F401, F403
 from .tracker import *  # noqa: F401, F403
